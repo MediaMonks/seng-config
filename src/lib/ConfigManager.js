76455738e521cfa2a751e5dd6ed7b52eedc2863a
Jesse Linthorst <jesse@mediamonks.com>
2017-04-06 15:52:24 +0200

    Update generated TypeScript 2.2.x files

--- src/lib/ConfigManager.js
+++ src/lib/ConfigManager.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var deepAssign = require('deep-assign');
 /**
  * @namespace app.config
@@ -336,5 +337,4 @@ var ConfigManager = (function () {
     };
     return ConfigManager;
 }());
-Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ConfigManager;
